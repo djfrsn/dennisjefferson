11c0c8cb8437d2f6e13235ae802563972ad24a0b
Dennis Jefferson <dennismjefferson@gmail.com>
2018-08-29 16:30:20 -0700

    HeroIntro progress

--- components/portfolio/components/HeroIntro/index.js
+++ components/portfolio/components/HeroIntro/index.js
@@ -6,7 +6,9 @@ const cx = classNames.bind(styles);
 
 const HeroIntro = () => (
   <div className={cx("hero__intro")}>
-    <h1 className={cx("hero__text_large")}>Lets buildin an amazing product</h1>
+    <h1 className={cx("hero__text_large")}>
+      Lets buildin an amazing product together
+    </h1>
     <h2 className={cx("hero__text_medium")}>
       Meet me at the intersection of modern web technology and design
     </h2>
